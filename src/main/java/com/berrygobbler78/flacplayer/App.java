bf23912f1156e7cba26dfabf15dc155cb944b4a2
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-07 20:40:29 -0700

    1. Added logger 2. Cleaned up code

--- src/main/java/com/berrygobbler78/flacplayer/App.java
+++ src/main/java/com/berrygobbler78/flacplayer/App.java
@@ -2,12 +2,17 @@ package com.berrygobbler78.flacplayer;
 
 import java.io.*;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.berrygobbler78.flacplayer.userdata.Playlist;
 import com.berrygobbler78.flacplayer.userdata.References;
 import com.berrygobbler78.flacplayer.util.FileUtils;
+import com.berrygobbler78.flacplayer.Constants.FXML_PATHS;
+
 import com.pixelduke.window.ThemeWindowManagerFactory;
 import com.pixelduke.window.Win11ThemeWindowManager;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -23,53 +28,59 @@ import org.controlsfx.dialog.Wizard;
 import org.controlsfx.dialog.WizardPane;
 
 public class App extends Application {
-    public static MusicPlayer musicPlayer = new MusicPlayer();
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
     public static References references;
 
-    private static final File refrencesFile = new File(
-            "src/main/resources/com/berrygobbler78/flacplayer/cache/References.ser");
+    private static final String referencesPath =
+            "src/main/resources/com/berrygobbler78/flacplayer/cache/references.ser";
 
     private static Stage primaryStage;
 
-    void main(String[] args) {
-        System.out.println("Hello World!");
+    void main() {
         launch();
     }
 
     @Override
-    public void start(Stage stage) throws IOException, ClassNotFoundException {
+    public void start(Stage stage) throws IOException {
         deleteTempFile();
 
         // Checks if userData already exists, if not prompt for new directory
+        File referencesFile = new File(referencesPath);
 
-        if(refrencesFile.exists()){
-            FileInputStream fis;
-            ObjectInputStream ois;
-            fis = new FileInputStream(refrencesFile);
-            ois = new ObjectInputStream(fis);
+        try {
+            FileInputStream fis = new FileInputStream(referencesFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
             references = (References) ois.readObject();
 
             fis.close();
             ois.close();
-        } else {
+
+            LOGGER.log(Level.INFO, "Previous references loaded.");
+        } catch (Exception e) {
             references = new References();
+
             setupWizard();
 
-            FileOutputStream fos = new FileOutputStream(refrencesFile);
+            FileOutputStream fos = new FileOutputStream(referencesFile);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+
             oos.writeObject(references);
+
             oos.close();
             fos.close();
 
+            LOGGER.log(Level.INFO, "Created new references.");
         }
 
-        references.clearPlaylists();
         loadPlaylists();
 
-        // For coloring window border
-        Win11ThemeWindowManager themeWindowManager = (Win11ThemeWindowManager) ThemeWindowManagerFactory.create();
+        Win11ThemeWindowManager themeWindowManager =
+                (Win11ThemeWindowManager) ThemeWindowManagerFactory.create(); // For coloring window border
 
-        FXMLLoader fxmlLoader =  new FXMLLoader(new File("src/main/resources/com/berrygobbler78/flacplayer/fxml/revised.fxml").getAbsoluteFile().toURI().toURL());
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(new File(FXML_PATHS.MAIN.get()).getAbsoluteFile().toURI().toURL());
 
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(Objects.requireNonNull(App.class.getResource("css/styles.css")).toExternalForm());
@@ -77,69 +88,86 @@ public class App extends Application {
         primaryStage = stage;
         primaryStage.setTitle("BerryBush");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(Constants.IMAGES.BERRIES.get());
         primaryStage.show();
-        primaryStage.setOnCloseRequest(event -> {
-            saveReferences();
-        });
+        primaryStage.setOnCloseRequest(_ -> saveUserData());
 
         themeWindowManager.setWindowFrameColor(primaryStage, Color.web("#121212"));
         themeWindowManager.setDarkModeForWindowFrame(primaryStage, true);
 
-        musicPlayer.setController(fxmlLoader.getController());
+        MusicPlayer.setController(fxmlLoader.getController());
+
+        LOGGER.log(Level.FINE, "Stage created.");
     }
 
     public void saveUserData() {
         savePlaylists();
         saveReferences();
+
+        LOGGER.log(Level.INFO, "User data saved.");
     }
 
     public static void saveReferences() {
         references.clearPlaylists();
 
         try {
-            FileOutputStream fos = new FileOutputStream(refrencesFile);
+            FileOutputStream fos = new FileOutputStream(referencesPath);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
+
             oos.writeObject(references);
+
             oos.close();
             fos.close();
         } catch (IOException e) {
-            System.err.println("Could not save references." + e);
+            LOGGER.log(Level.SEVERE, "Could not save references.", e);
         }
-
     }
 
     public static void savePlaylists() {
         try{
             for(Playlist playlist : references.getPlaylists()){
-                FileOutputStream fos = new FileOutputStream(new File("src/main/resources/com/berrygobbler78/flacplayer/cache/playlists/" + playlist.getName().toLowerCase().replace(" ", "-") + ".ser"));
+                FileOutputStream fos = new FileOutputStream("src/main/resources/com/berrygobbler78/flacplayer/cache/playlists/" + playlist.getName().toLowerCase().replace(" ", "-") + ".ser");
                 ObjectOutputStream oos = new ObjectOutputStream(fos);
+
                 oos.writeObject(playlist);
+
                 oos.close();
                 fos.close();
             }
 
+            LOGGER.log(Level.INFO, "Playlists saved.");
         } catch(IOException e){
-            System.err.println("Could not save playlists." + e);
+            LOGGER.log(Level.SEVERE, "Could not save playlists.", e);
+        }
+    }
 
+    public static void deletePlaylist(Playlist playlist) {
+        File playlistFile = new File("src/main/resources/com/berrygobbler78/flacplayer/cache/playlists/" +  playlist.getName().toLowerCase().replace(" ", "-") + ".ser");
+
+        if(playlistFile.delete()) {
+            LOGGER.log(Level.INFO, "Deleted playlist." +  playlist.getName());
+        } else {
+            LOGGER.log(Level.WARNING, "Could not delete playlist." +  playlist.getName());
         }
     }
 
     public void loadPlaylists() {
         try {
-            for (File file : new File("src/main/resources/com/berrygobbler78/flacplayer/cache/playlists").listFiles()) {
+            for (File file : Objects.requireNonNull(new File("src/main/resources/com/berrygobbler78/flacplayer/cache/playlists").listFiles())) {
                 if (file.getName().endsWith(".ser")) {
-                    FileInputStream fis;
-                    ObjectInputStream ois;
-                    fis = new FileInputStream(file);
-                    ois = new ObjectInputStream(fis);
+                    FileInputStream fis = new FileInputStream(file);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+
                     references.addPlaylist((Playlist) ois.readObject());
 
                     fis.close();
                     ois.close();
+
+                    LOGGER.log(Level.INFO, "Loaded playlist " + file.getName());
                 }
             }
         } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Could not load playlists." + e);
+            LOGGER.log(Level.SEVERE, "Could not load playlists." + e);
         }
     }
 
@@ -148,23 +176,23 @@ public class App extends Application {
     }
 
     static void deleteTempFile() {
-        try {
-            File tempFile = new File("src/main/resources/com/berrygobbler78/flacplayer/cache/temp.wav");
+        File tempFile = new File("src/main/resources/com/berrygobbler78/flacplayer/cache/temp.wav");
 
-            if(tempFile.exists()) {
-                try {
-                    musicPlayer.closeMediaPlayer();
-                } catch (Exception e) {
-                    System.out.println("No media player to close");
-                }
-                if(tempFile.delete()) {
-                    System.out.println("Deleted temp file");
-                } else {
-                    System.out.println("Failed to delete temp file");
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("No temp file to delete!");
+        if(!tempFile.exists()) {
+            LOGGER.log(Level.INFO, "Temp file does not exist.");
+            return;
+        }
+
+        try {
+            MusicPlayer.closeMediaPlayer();
+        } catch (NullPointerException e) {
+            LOGGER.log(Level.FINE, "No media player to be closed.");
+        }
+
+        if(tempFile.delete()) {
+            LOGGER.log(Level.INFO, "Deleted temp file.");
+        } else {
+            LOGGER.log(Level.WARNING, "Failed to delete temp file.");
         }
     }
 
@@ -175,43 +203,42 @@ public class App extends Application {
         Label question1 = new Label("Enter Directory Location:");
 
         TextField textField1 = new TextField();
-        textField1.setEditable(true);
-        textField1.setPromptText("Select Directory");
-        textField1.setMinWidth(300.0);
-        textField1.setMaxHeight(10.0);
-        textField1.setMaxWidth(300.0);
+            textField1.setEditable(true);
+            textField1.setPromptText("Select Directory");
+            textField1.setMinWidth(300.0);
+            textField1.setMaxHeight(10.0);
+            textField1.setMaxWidth(300.0);
 
         Button button1 = new Button();
-        button1.setOnAction(event -> {
-            textField1.setText(FileUtils.openDirectoryChooser(primaryStage, "Choose directory", "C://").getAbsolutePath());
-        });
-        button1.setText("Open Explorer");
+            button1.setOnAction(_ ->
+                    textField1.setText(FileUtils.openDirectoryChooser(primaryStage, "Choose directory", "C://").getAbsolutePath()));
+            button1.setText("Open Explorer");
 
         HBox hbox1 = new HBox();
-        hbox1.getChildren().addAll(textField1, button1);
-        hbox1.setSpacing(10.0);
+            hbox1.getChildren().addAll(textField1, button1);
+            hbox1.setSpacing(10.0);
 
         VBox vbox1 = new VBox();
-        vbox1.getChildren().addAll(question1, hbox1);
+            vbox1.getChildren().addAll(question1, hbox1);
 
         WizardPane page1 = new WizardPane();
-        page1.setContent(vbox1);
+            page1.setContent(vbox1);
 
         // Page2
         Label question2 = new Label("Enter Username:");
 
         TextField textField2 = new TextField();
-        textField2.setEditable(true);
-        textField2.setPromptText("Username...");
-        textField2.setMinWidth(300.0);
-        textField2.setMaxHeight(10.0);
-        textField2.setMaxWidth(300.0);
+            textField2.setEditable(true);
+            textField2.setPromptText("Username...");
+            textField2.setMinWidth(300.0);
+            textField2.setMaxHeight(10.0);
+            textField2.setMaxWidth(300.0);
 
         VBox vbox2 = new VBox();
-        vbox2.getChildren().addAll(question2, textField2);
+            vbox2.getChildren().addAll(question2, textField2);
 
         WizardPane page2 = new WizardPane();
-        page2.setContent(vbox2);
+            page2.setContent(vbox2);
 
         wizard.setUserData(textField2.getText());
 
@@ -224,8 +251,9 @@ public class App extends Application {
             if (result == ButtonType.FINISH) {
                 references.setRootDirectoryPath(textField1.getText());
                 references.setUserName(textField2.getText());
+
+                saveReferences();
             }
         });
     }
-
 }
