5ca5a73f33e442742b47dcb6e625b24eaafdbbe9
berrygobbler78 <w.garis.2006@gmail.com>
2026-02-19 12:33:30 -0700

    1. Makes tempfile for current art

--- src/main/java/com/berrygobbler78/flacplayer/util/MediaTransport.java
+++ src/main/java/com/berrygobbler78/flacplayer/util/MediaTransport.java
@@ -1,12 +1,21 @@
 package com.berrygobbler78.flacplayer.util;
 
 import io.github.selemba1000.*;
+import javafx.concurrent.Task;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class MediaTransport {
     private final JMTC JMTC_OBJECT;
     private final JMTCCallbacks CALLBACKS;
+
+    private String currentSong = "";
+    private Path copied;
 
     public MediaTransport(String playerName, String playerPath, MusicPlayer musicPlayer) {
         JMTC_OBJECT = JMTC.getInstance(
@@ -18,9 +27,18 @@ public class MediaTransport {
         CALLBACKS.onPause = musicPlayer::pause;
         CALLBACKS.onStop = musicPlayer::stop;
 
+        CALLBACKS.onNext = musicPlayer::next;
+        CALLBACKS.onPrevious = musicPlayer::previous;
+
+        CALLBACKS.onLoop = musicPlayer::setRepeatStatus;
+        CALLBACKS.onShuffle = musicPlayer::setShuffleStatus;
+
+        CALLBACKS.onVolume = musicPlayer::setVolume;
+
         JMTC_OBJECT.setCallbacks(CALLBACKS);
         JMTC_OBJECT.setMediaType(JMTCMediaType.Music);
-        JMTC_OBJECT.setPosition(100L);
+        JMTC_OBJECT.setPosition(400L);
+        JMTC_OBJECT.setEnabled(true);
     }
 
     public void setEnabled(boolean enabled) {
@@ -28,19 +46,44 @@ public class MediaTransport {
     }
 
     public void setProperties(String songTitle, String songArtist, String parentTitle, String parentArtist, int tracks, int index, File coverArt) {
-        JMTC_OBJECT.setMediaProperties(
-                new JMTCMusicProperties(
-                        songTitle,
-                        songArtist,
-                        parentTitle,
-                        parentArtist,
-                        new String[]{},
-                        tracks,
-                        index,
-                        coverArt
-                )
-        );
-        JMTC_OBJECT.updateDisplay();
+        Task<Void> update = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                if (!currentSong.equals(songTitle)) {
+                    try {
+                        Path original = coverArt.toPath();
+                        File currentArt = File.createTempFile("currentArt", "");
+                        currentArt.deleteOnExit();
+                        currentArt.setReadable(false, false);
+                        currentArt.setWritable(false, false);
+                        currentArt.deleteOnExit();
+                        copied = currentArt.toPath();
+                        Files.copy(original, copied, StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+
+                JMTC_OBJECT.setMediaProperties(
+                        new JMTCMusicProperties(
+                                songTitle,
+                                songArtist,
+                                parentTitle,
+                                parentArtist,
+                                new String[]{},
+                                1,
+                                1,
+                                new File(copied.toString())
+                        )
+                );
+                JMTC_OBJECT.updateDisplay();
+
+                return null;
+            }
+        };
+
+        new Thread(update).start();
     }
 
     public void setTimeline(long start, long end, long seekStart, long seekEnd) {
@@ -59,5 +102,6 @@ public class MediaTransport {
         JMTC_OBJECT.setPlayingState(state);
         JMTC_OBJECT.updateDisplay();
     }
+
 
 }
